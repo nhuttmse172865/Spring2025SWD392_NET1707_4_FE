c4589fb0ae5c03c7a2d0d6c2c25fc2a41b9a6189
nam14012003 <namnpse173557@fpt.edu.vn>
2025-02-14 14:46:33 +0700

    Code Ui Booking  & Contact

--- src/components/customer/header/Header.jsx
+++ src/components/customer/header/Header.jsx
@@ -6,7 +6,11 @@ import Search from "../../common/search/Search";
 import { useNavigate } from "react-router-dom";
 import HeaderVerified from "./verified/HeaderVerified";
 
-const Header = ({ isShowSearch = false, isShowButtonLogin = false, isVerified = false }) => {
+const Header = ({
+  isShowSearch = false,
+  isShowButtonLogin = false,
+  isVerified = false,
+}) => {
   const navigate = useNavigate();
   return (
     <div className="header-container">
@@ -15,20 +19,43 @@ const Header = ({ isShowSearch = false, isShowButtonLogin = false, isVerified = 
           <li onClick={() => navigate("/")} style={{ cursor: "pointer" }}>
             Home
           </li>
-          <li onClick={() => navigate("/customer-service")} style={{ cursor: "pointer" }}>
+          <li
+            onClick={() => navigate("/customer-service")}
+            style={{ cursor: "pointer" }}
+          >
             Services
           </li>
           <li>Price</li>
-          <li>Booking</li>
-          <li>Blog</li>
-          <li>Contact us</li>
+          <li
+            onClick={() => navigate("/booking")}
+            style={{ cursor: "pointer" }}
+          >
+            Booking
+          </li>
+          <li onClick={() => navigate("/blog")} style={{ cursor: "pointer" }}>
+            Blog
+          </li>
+          <li
+            onClick={() => navigate("/contact")}
+            style={{ cursor: "pointer" }}
+          >
+            Contact us
+          </li>
         </ul>
         <div className="header-logo">
           <img src={IMAGES.logo} alt="" />
         </div>
         <div className="items-center">
           {isShowSearch && <Search />}
-          {isShowButtonLogin && <ElevatedButton handleOnclick={() => navigate("/login")} text="Login" width="150px" height="40px" rounded=".375rem" />}
+          {isShowButtonLogin && (
+            <ElevatedButton
+              handleOnclick={() => navigate("/login")}
+              text="Login"
+              width="150px"
+              height="40px"
+              rounded=".375rem"
+            />
+          )}
           {isVerified && <HeaderVerified />}
         </div>
       </div>
---
+++ src/pages/customer/customerBooking/Booking.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import BookingPage from "../../../components/customer/booking/BookingPage";
+const Customer = () => {
+  return (
+    <div className="booking-container container mx-auto">
+      <BookingPage />
+    </div>
+  );
+};
+
+export default Customer;
