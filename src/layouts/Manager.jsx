3a7553fa275fa4426e4e48b6076946b819431ca8
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-03-17 15:54:58 +0700

    authorize

--- src/layouts/Manager.jsx
+++ src/layouts/Manager.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Navigation from "../components/manager/navigation/Navigation";
 import Header from "../components/manager/header/Header";
 import { Outlet } from "react-router-dom";
+import Authorization from "../middleware/Authorization";
+import ROLES from "../constants/role";
 
 const Manager = () => {
   return (
-    <div className="flex h-[100vh] gap-x-6 bg-[#F7F7F7]">
-      <div className="min-w-[250px] max-w-[300px] h-full">
-        <Navigation />
+    <Authorization requiredRole={ROLES.MANAGER}>
+      <div className="flex h-[100vh] gap-x-6 bg-[#F7F7F7]">
+        <div className="min-w-[250px] max-w-[300px] h-full">
+          <Navigation />
+        </div>
+        <div className="w-full pr-6">
+          <Header />
+          <Outlet />
+        </div>
       </div>
-      <div className="w-full pr-6" >
-        <Header />
-        <Outlet />
-      </div>
-    </div>
+    </Authorization>
   );
 };
 
