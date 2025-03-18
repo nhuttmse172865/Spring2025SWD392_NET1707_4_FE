e673390b9c9ab4e6c6e5d58dbfbc160a651e6e44
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-03-18 17:05:12 +0700

    hotfix

---
+++ src/constants/dashboard.js
@@ -0,0 +1,67 @@
+const ANALYSIS_ITEM = [
+  {
+    title: "Transactions",
+  },
+  {
+    title: "Customer",
+  },
+  {
+    title: "Revenue",
+  },
+];
+
+const LIST_TITLE_POPULAR_SERVICES = [
+  {
+    name: "No.",
+    column: 1.5,
+  },
+  {
+    name: "Services",
+    column: 5,
+  },
+  {
+    name: "Number Uses",
+    column: 3,
+  },
+  {
+    name: "Price",
+    column: 2.5,
+  },
+];
+
+const LIST_TITLE_TRANSACTIONS = [
+  {
+    name: "No.",
+    column: 1,
+  },
+  {
+    name: "Customer",
+    column: 3.2,
+  },
+  {
+    name: "Service",
+    column: 3.3,
+  },
+
+  {
+    name: "Date",
+    column: 2,
+  },
+  {
+    name: "Price",
+    column: 1.5,
+  },
+
+  {
+    name: "Status",
+    column: 1,
+  },
+];
+
+const DASHBOARD = {
+  ANALYSIS_ITEM,
+  LIST_TITLE_POPULAR_SERVICES,
+  LIST_TITLE_TRANSACTIONS,
+};
+
+export default DASHBOARD;
